ede4580ec588fbbc95fed496b09f6d12bfdb6f2f
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-04-21 22:13:59 +0200

    First commit

---
+++ src/main/java/com/kodilla/tictactoe_kodilla/TicTacToeKodillaApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.tictactoe_kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeKodillaApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(TicTacToeKodillaApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/kodilla/tictactoe_kodilla/TicTacToeKodillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.tictactoe_kodilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeKodillaApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
