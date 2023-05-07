623dd7a94b252f030a92419487a249fe6ab52c9e
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-05-07 20:05:42 +0200

    Working game logic for 3x3 board

--- src/main/java/com/kodilla/tictactoe_kodilla/UserInput.java
+++ src/main/java/com/kodilla/tictactoe_kodilla/UserInput.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 
 public class UserInput {
 
-   private Scanner s = new Scanner(System.in);
+   private final Scanner scanner = new Scanner(System.in);
 
    public char selectCharacter() {
 
       char charPlayer1;
       //check if string length = 1
       while (true) {
-         String temp = s.nextLine().toLowerCase();
+         String temp = scanner.nextLine().toUpperCase();
          if (temp.length() == 1) {
             //convert to char
             charPlayer1 = temp.charAt(0);
             //check if x/o
-            if (charPlayer1 == 'o' || charPlayer1 == 'x') {
+            if (charPlayer1 == 'O' || charPlayer1 == 'X') {
                return charPlayer1;
             }
          }
-         System.out.println("Can't accept this, choose between x and o.");
+         System.out.println("Can't accept this, choose between X and O.");
       }
    }
 
    public Coordinates getMove() {
       while (true) {
-         String temp = s.nextLine();
+         String temp = scanner.nextLine();
          try {
             int tempInt = Integer.parseInt(temp);
             int column = tempInt % 10;
